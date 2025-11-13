dc77f0e8b3ed7841f5eb6982553f4b39891d0456
Ajilx3 <ajilsaji0299@gmail.com>
2025-11-13 21:40:49 +0530

    completed the day 1 of python

---
+++ my_solution/day_1/varibles.py
@@ -0,0 +1,12 @@
+firstname='ajil'
+lastname='saji'
+age=21
+country='India'
+
+#single line variable declaration
+
+firstname,lastname,age,country='ajil','saji',21,'india'
+
+print('first name',firstname)
+print('second name',lastname)
+print('age',age)
