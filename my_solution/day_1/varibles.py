ebc7211f83c8e9b49fde4ede5c2d20fba92bccaa
Ajilx3 <ajilsaji0299@gmail.com>
2025-11-13 21:50:06 +0530

    level 1 of the excercise in the day 1 has completed

--- my_solution/day_1/varibles.py
+++ my_solution/day_1/varibles.py
@@ -10,3 +10,16 @@ firstname,lastname,age,country='ajil','saji',21,'india'
 print('first name',firstname)
 print('second name',lastname)
 print('age',age)
+
+
+#exercise level1
+
+firstname='ajil'
+lastname='saji'
+fullname='ajil saji'
+country='India'
+city='Kottayam'
+age=21
+year=2025
+is_married='no'
+is_true='yes'
